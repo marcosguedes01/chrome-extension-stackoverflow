57323cd183336aabcbc7ed2e48a17fc1269f0f0c
Marcos Guedes <mguedes@growin.pt>
2019-12-09 10:53:29 +0000

    Arquivos Iniciais

---
+++ js/content.js
@@ -0,0 +1,6 @@
+console.log($(".answer.accepted-answer .js-vote-count").text());
+
+// Your web app's Firebase configuration
+
+
+// https://www.originate.com/thinking/stories/building-a-chrome-extension-using-firebase-real-time-database/
